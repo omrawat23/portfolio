ad124e7f731d1e4470b4e03cc6286b2447c9fff5
omrawat23 <57662461+omrawat23@users.noreply.github.com>
2024-05-18 05:16:08 +0530

    first commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,80 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useLoader(FBXLoader, "./desktop_pc/1.fbx");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={1} groundColor="black" />
+      {/* <spotLight
+        position={[20, 50, 10]}
+        angle={0}
+        penumbra={0.5}
+        intensity={0.5}
+        castShadow
+        shadow-mapSize={1024}
+      /> */}
+      <pointLight intensity={0.1} />
+      <primitive
+        object={computer}
+        scale={isMobile ? [2.5, 2.5, 2.5] : [3.5, 3.5, 3.5]} // Adjusted scale to make it smaller
+        position={isMobile ? [2.2, -3.3, -0.1] : [5, -3, -1.5]} // Adjusted position to move it upwards
+        rotation={[-Math.PI / 20, 1.1, 0.2]} // Rotate object to stand vertically
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
